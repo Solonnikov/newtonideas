bc00172825c72393e266e9e699324ee12c02da8d
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2018-02-24 16:45:57 +0200

    added add/id/list new components, new model, bootswatch theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
+import { NewsDetailsComponent } from './components/news-details/news-details.component';
+import { AddNewsComponent } from './components/add-news/add-news.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NewsListComponent,
+    NewsDetailsComponent,
+    AddNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: NewsListComponent },
+      { path: 'add', component: AddNewsComponent },
+      { path: ':id', component: NewsDetailsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
