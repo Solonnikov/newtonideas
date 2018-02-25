f9df9144606f1866308d300341607da2302f083f
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2018-02-26 01:17:20 +0200

    fixed category filter to work with async data

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any[], args?: any): any {
+    if (!value || !value.length) return [];
     return value.filter(item =>
       item.category.toUpperCase().indexOf(args.toUpperCase()) !== -1);
   }
